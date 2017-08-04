40b9ba06e8c53fb2b3e6937de96401f81d4afa59
nhmnmh1996 <nhmnmh1996@gmail.com>
2017-08-05 03:02:36 +0700

    ggwp

--- module5/src/public/signup/signup.controller.js
+++ module5/src/public/signup/signup.controller.js
@@ -4,13 +4,13 @@
   angular.module('common')
   .controller('SignUpController',SignUpController);
 
-  //SignUpController.$inject=[];
-  function SignUpController() {
-    var signupCtrl=this;
-    signupCtrl.users=[];
-    signupCtrl.submit=function () {
-      signupCtrl.completed=true;
-      signupCtrl.users.push(reg.user);
+  SignUpController.$inject=['$rootScope'];
+  function SignUpController($rootScope) {
+    var $signupCtrl=this;
+    $signupCtrl.users=[];
+    $signupCtrl.submit=function () {
+      $signupCtrl.completed=true;
+      $signupCtrl.users.push($signupCtrl.user);
     }
 
   }
