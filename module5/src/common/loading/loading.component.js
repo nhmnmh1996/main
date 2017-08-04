783bd224b878ba1352d8424230b594862e9c00ec
NHMNMH <nhmnmh1996@coursera-somethingn.com>
2017-08-05 02:49:02 +0700

    ggwp

--- module5/src/common/loading/loading.component.js
+++ module5/src/common/loading/loading.component.js
@@ -5,6 +5,10 @@ angular.module('common')
 .component('loading', {
   template: '<img src="images/spinner.svg" ng-if="$ctrl.show">',
   controller: LoadingController
+})
+.component('signUp', {
+  template: 'src/public/signup/signup.html',
+  controller: SignUpController
 });
 
 
---
+++ module5/src/public/signup/signup.controller.js
@@ -0,0 +1,17 @@
+(function () {
+  'use strict';
+
+  angular.module('public')
+  .controller('SignUpController',SignUpController);
+
+  //SignUpController.$inject=[];
+  function SignUpController() {
+    var signupCtrl=this;
+    signupCtrl.users=[];
+    signupCtrl.submit=function () {
+      signupCtrl.completed=true;
+      signupCtrl.users.push(reg.user);
+    }
+
+  }
+})();
